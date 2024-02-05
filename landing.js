9dcebf1347d42972e40fd55180112564a4633bb1
Utkarsh Tripathi <117009064+ut-stack@users.noreply.github.com>
2024-02-05 14:12:40 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,4 @@
+ // Add JavaScript to handle the "Home" button click event
+ document.getElementById("homeButton").addEventListener("click", function() {
+    window.location.href = "home.html";
+  });
